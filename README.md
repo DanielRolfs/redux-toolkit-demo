cf2a4f3edd357620c850d012844feb5280959387
DanielRolfs <danielrolfs@icloud.com>
2023-01-04 13:19:16 +0100

    add logger to middleware

---
+++ README.md
@@ -0,0 +1,2 @@
+to run the code enter in the terminal:
+$ node index
--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const icecreamActions = require('./features/icecream/icecreamSlice').icecreamAct
 
 console.log('Initial state', store.getState())
 
-const unsubscribe = store.subscribe(() => {
-    console.log('Updated state', store.getState())
-})
+const unsubscribe = store.subscribe(() => {})
 
 store.dispatch(cakeActions.ordered(2))
 store.dispatch(cakeActions.restocked(3))
