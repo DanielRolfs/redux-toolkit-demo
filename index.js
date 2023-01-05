0ed2f5965a2e9dbed05f761d58ff6abbab688680
DanielRolfs <danielrolfs@icloud.com>
2023-01-05 10:26:03 +0100

    adding asyncThunk

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const store = require('./app/store')
 const cakeActions = require('./features/cake/cakeSlice').cakeActions
 const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions
+const fetchUsers = require('./features/user/userSlice').fetchUsers
 
 console.log('Initial state', store.getState())
 
@@ -8,8 +9,10 @@ const unsubscribe = store.subscribe(() => {
     console.log('Updating state', store.getState())
 })
 
-store.dispatch(cakeActions.ordered(2))
-store.dispatch(cakeActions.restocked(3))
+store.dispatch(fetchUsers())
+
+/* store.dispatch(cakeActions.ordered(2))
+store.dispatch(cakeActions.restocked(3)) */
 
 /* store.dispatch(icecreamActions.ordered(3))
 store.dispatch(icecreamActions.restocked(2)) */
@@ -18,4 +21,4 @@ store.dispatch(icecreamActions.restocked(2)) */
 
 
 
-unsubscribe()
+/* unsubscribe() */
